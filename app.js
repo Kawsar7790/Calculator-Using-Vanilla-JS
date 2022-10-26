baa5f7467dc505ef09193eb054260f7b072ca2bf
kawsarselise <kawsaralam112307@gmail.com>
2022-10-26 18:58:27 +0600

    0.2 Logic Implemented

--- app.js
+++ app.js
@@ -4,11 +4,34 @@ const calcvalues = document.querySelectorAll('.calc');
 const dotvalue = document.querySelector('.dot');
 const clear = document.querySelector('.clear');
 const display = document.querySelector('#display');
+const recheck = document.querySelector('.recheck');
 const zero = '0';
 
 if (display.value.length == 0) {
     display.value = "0";
 }
+
+
+digitvalues.forEach( digitvalue => {
+    digitvalue.addEventListener('click' , function(e) {
+        let display_value = display.value;
+
+        if(display_value.length > 1 && display_value.charAt(0) === '0' && display_value.charAt(1) === '.'){
+            console.log("Latest");
+            display_value += digitvalue;
+            console.log(display_value);
+        }
+        else{
+            let zerovalue = display_value.charAt(0);
+            let replaced_value = display_value.replace(zerovalue,'');
+            display.value = replaced_value;
+        }
+       
+    })
+});
+
+
+
 
 digitvalues.forEach( digitvalue => {
     digitvalue.addEventListener('click' , function() {
@@ -17,18 +40,8 @@ digitvalues.forEach( digitvalue => {
     })
 });
 
-digitvalues.forEach( digitvalue => {
-    digitvalue.addEventListener('click' , function(e) {
-        let display_value = display.value;
-        if (display_value.length > 1 && display_value.charAt(0) === '0' && e.target.value != '.' ) {
-            let zerovalue = display_value.charAt(0);
-            let replaced_value = display_value.replace(zerovalue,'');
-            display.value = replaced_value;
-        }
-    })
-});
 
-let recheck = document.querySelector('.recheck');
+
 recheck.addEventListener('click' , function() {
     const deleted_val= display.value.substr(0,display.value.length-1);
     display.value = deleted_val;
@@ -41,55 +54,14 @@ recheck.addEventListener('click' , function() {
 
 clear.addEventListener('click' , function() {
     display.value = "0";
-    if (display.value.charAt(1) === 1 ) {
-        
-    }
 });
 
 
 
-dotvalue.addEventListener('click' , function(e) {
-    display.value += '0.';
+dotvalue.addEventListener('click' , function() {
+    display.value += dotvalue.value;
 });
 
-// calcvalues.forEach( calcvalue => {
-//     calcvalue.addEventListener('click' , function() {
-//             let operatorvalue = calcvalue.value;
-//             display.value += operatorvalue;
-//             for (let i = 0; i < display.value.length; i++) {
-//                 for (let j = 0; j < action_opt.length; j++) {
-//                     if (display.value.includes(action_opt[j])) {
-//                         calcvalues.forEach( calcvalue => {
-//                             calcvalue.addEventListener('click' , function(e) {
-//                                 let opt_pos = display.value.indexOf(action_opt[j]);
-//                                 let signvalue = display.value.charAt(opt_pos);
-//                                 console.log(signvalue);
-//                                 let replaced_sign_value = display.value.replace(signvalue,'');
-//                                 display.value = replaced_sign_value;
-//                             })
-//                         });
-//                     }    
 
-//                         digitvalues.forEach( digitvalue => {
-//                             digitvalue.addEventListener('click' , function() {
-//                                 for (const calcvalue of calcvalues) {
-//                                     calcvalue.disabled = true;
-//                                   }
-//                             })
-//                         });
-                        
-//                         recheck.addEventListener('click' , function() {
-//                             if (display.value.slice(-1) == action_opt[j]) {
-//                                 for (const calcvalue of calcvalues) {
-//                                     calcvalue.disabled = false;
-//                                   }
-//                             }
-//                         });
-
-//                     }
-//                 }
-              
-//     })
-// });
 
  
